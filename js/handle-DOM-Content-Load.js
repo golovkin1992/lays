09b5bc2da01798bb6781ae7b4378fced99e8fa31
golovkin1992 <golova92-69@yandex.ru>
2019-03-31 23:08:13 +0300

    scroll gogle chrome

--- js/handle-DOM-Content-Load.js
+++ js/handle-DOM-Content-Load.js
@@ -1,4 +1,7 @@
 // Прокрутка страницы в начало
+	if ('scrollRestoration' in history) { 
+    	history.scrollRestoration = 'manual'; 	
+		} 
  document.addEventListener("DOMContentLoaded", function()
  { 
   document.body.scrollTop = document.documentElement.scrollTop = 0;
